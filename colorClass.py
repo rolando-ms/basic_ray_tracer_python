7aa890b173dbb16f6146ef37361c3f574724b3b2
rolando-ms <rolando.moralessuarez@gmail.com>
2022-06-29 18:51:52 +0200

    Sphere with gamma correction

--- colorClass.py
+++ colorClass.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from CommonUtilities import *
 
 
@@ -7,9 +9,9 @@ def write_color(colour: Vec3, samples_per_pixel: int) -> str:
     b = colour.get_z()
 
     scale = 1.0 / samples_per_pixel
-    r *= scale
-    g *= scale
-    b *= scale
+    r = np.sqrt(scale * r)
+    g = np.sqrt(scale * g)
+    b = np.sqrt(scale * b)
 
     return  str(int(256 * np.clip(r, 0.0, 0.999))) + ' ' + \
             str(int(256 * np.clip(g, 0.0, 0.999))) + ' ' + \
