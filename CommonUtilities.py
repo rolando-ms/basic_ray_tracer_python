06b8f03887f30cbcd56f51c350a8b22b7bf5f487
rolando-ms <rolando.moralessuarez@gmail.com>
2022-06-29 11:13:50 +0200

    Added common libraries

---
+++ CommonUtilities.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+from vec3Class import Vec3
+from vec3Class import vec3_dot
+from rayClass import Ray
+from hittableClass import Hittable
+from hittableClass import HitRecord
+from HittableListClass import HittableList
+
+color = Vec3
+point3 = Vec3
