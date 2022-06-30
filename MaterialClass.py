319b7bf30dedba875c40e8f88b522fff88f5d6b6
rolando-ms <rolando.moralessuarez@gmail.com>
2022-06-30 11:24:42 +0200

    Added Dielectric material

--- MaterialClass.py
+++ MaterialClass.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 from CommonUtilities import *
 color = Vec3
@@ -44,8 +45,26 @@ class Metal(Material):
         self.albedo = albedo
         self.fuzz = np.clip(fuzz, 0.0, 1.0)
 
-    def scatter(self, r_in: Ray, rec: HitRecord, attenuation: color, scattered: Ray) -> list[bool, float, Ray]:
+    def scatter(self, r_in: Ray, rec: HitRecord, attenuation: color, scattered: Ray) -> list[bool, color, Ray]:
         reflected = vec3_reflect(r_in.direction.unit_vector(), rec.normal)
         scattered = Ray(rec.p, reflected + self.fuzz*random_in_unit_sphere())
         attenuation = self.albedo
         return [vec3_dot(scattered.direction, rec.normal) > 0, attenuation, scattered]
+
+
+class Dielectric(Material):
+    def __init__(self, index_of_refraction: float):
+        self.index_of_refraction = index_of_refraction
+
+    def scatter(self, r_in: Ray, rec: HitRecord, attenuation: color, scattered: Ray) -> list[bool, color, Ray]:
+        attenuation = color(np.ones(3))
+        refraction_ratio = self.index_of_refraction
+        if rec.front_face:
+            refraction_ratio = 1.0 / self.index_of_refraction
+
+        unit_direction = r_in.direction.unit_vector()
+        refracted = vec3_refract(unit_direction, rec.normal, refraction_ratio)
+
+        scattered = Ray(rec.p, refracted)
+
+        return [True, attenuation, scattered]
