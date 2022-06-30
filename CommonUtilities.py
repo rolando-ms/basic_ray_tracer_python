8f35c88b1f13f800f87cac3bb338a66213f9ec36
rolando-ms <rolando.moralessuarez@gmail.com>
2022-06-30 10:34:00 +0200

    Updated files

--- CommonUtilities.py
+++ CommonUtilities.py
@@ -2,11 +2,12 @@
 import numpy as np
 from vec3Class import random_in_unit_sphere
 from vec3Class import random_unit_vector
+from vec3Class import vec3_reflect
 from vec3Class import Vec3
 from vec3Class import vec3_dot
 from rayClass import Ray
 from hittableClass import Hittable
-from hittableClass import HitRecord
+from MaterialClass import HitRecord
 from HittableListClass import HittableList
 
 color = Vec3
