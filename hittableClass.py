7ee2eeb3088cb3db4995ade3c1976b1d6e6f3d0f
rolando-ms <rolando.moralessuarez@gmail.com>
2022-06-30 10:30:45 +0200

    HitRecord moved to material class to avoid circular dependency

--- hittableClass.py
+++ hittableClass.py
@@ -1,20 +1,6 @@
 
-from CommonUtilities import *
-
-
-class HitRecord:
-    def __init__(self):
-        self.p = Vec3()
-        self.normal = Vec3()
-        self.t = 0.0
-        self.front_face = False
-
-    def set_face_normal(self, r: Ray, outward_normal: Vec3):
-        self.front_face = vec3_dot(r.direction, outward_normal) < 0
-        if self.front_face:
-            self.normal = outward_normal
-        else:
-            self.normal = -1*outward_normal
+from rayClass import Ray
+from MaterialClass import HitRecord
 
 
 class Hittable:
